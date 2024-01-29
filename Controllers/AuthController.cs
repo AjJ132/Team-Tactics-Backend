f39c0e58a0e931cdf28e188d94ff27a7c852e694
AJ Johnson <aj132@icloud.com>
2024-01-29 11:37:58 -0500

    Init Commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Team_Tactics_Backend.Database;
+
+namespace Team_Tactics_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IDbContextFactory<TeamTacticsDBContext> _contextFactory;
+
+        public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IDbContextFactory<TeamTacticsDBContext> contextFactory)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _contextFactory = contextFactory;
+        }
+
+    }
+
+}
---
+++ Database/TeamTacticsDBContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Team_Tactics_Backend.Database
+{
+    public class TeamTacticsDBContextFactory : IDbContextFactory<TeamTacticsDBContext>
+    {
+        private readonly DbContextOptions<TeamTacticsDBContext> _options;
+
+        public TeamTacticsDBContextFactory(DbContextOptions<TeamTacticsDBContext> options)
+        {
+            _options = options;
+        }
+
+        public TeamTacticsDBContext CreateDbContext()
+        {
+            return new TeamTacticsDBContext(_options);
+        }
+    }
+}
