3450e567912755b7164c05a533c8a28464559c42
ajjohnson <aj132@icloud.com>
2024-02-03 09:36:48 -0500

    Added Create endpoint on calendar controller

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,137 +1,142 @@
 
-﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
-using Team_Tactics_Backend.Database;
 using Team_Tactics_Backend.Models.Users;
+using Team_Tactics_Backend.Database;
+using Microsoft.AspNetCore.Authorization;
+using System.Globalization;
+using Team_Tactics_Backend.DTO;
 using TeamTacticsBackend.Models.CalendarEvents;
-using Team_Tactics_Backend.CalendarEventsData;
-using Team_Tactics_Backend.Controllers;
+using System.Diagnostics;
 
 
-namespace Team_Tactics_Backend.CalendarControllers{
+namespace Team_Tactics_Backend.CalendarControllers
+{
 
     [ApiController]
     [Route("[controller]")]
     public class CalendarController : ControllerBase
     {
+        private readonly UserManager<User> usermanager;
+        private readonly IDbContextFactory<TeamTacticsDBContext> contextFactory;
 
-        private readonly CALDAL dal;
-         private readonly UserManager<User> usermanage;
-
-       public CalendarController(CALDAL dal, UserManager<User> UserManager)
+        public CalendarController(UserManager<User> UserManager, IDbContextFactory<TeamTacticsDBContext> contextFactory)
         {
-            dal = dal;
-            usermanage = UserManager;
-        }
-         public IActionResult Description(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @event = dal.GetEvent((Guid)id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-           // return event in the calendar;
-        }
-        
-          public IActionResult Create()
-        {
-            //return new event in calendar;
+            this.usermanager = UserManager;
+            this.contextFactory = contextFactory;
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        
-        public async Task<IActionResult> Create(CalendarEvent ce, IFormCollection form)
+        //Create calendar event
+        [HttpPost("create")]
+        [Authorize]
+        public async Task<IActionResult> CreateCalendarEvent([FromBody] NewCalendarEventDTO model)
         {
             try
             {
-               //create an event dal.CreateEvent(form);
-                //send an alert saying event was created TempData["Alert"] = "Success! An event was created!";
-                return RedirectToAction("Index");
-            } catch(Exception ex)
-            {
-               //send an error message if an error occured ViewData["Alert"] = "An error occurred: " + ex.Message;
-               // return the original event;
-            }
-        }
+                //Get the current user
+                var identUser = await usermanager.GetUserAsync(User);
 
-        // GET: Event/Edit/
-        public IActionResult Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+                if (identUser == null)
+                {
+                    return BadRequest("User not found");
+                }
 
-            var @event = dal.GetEvent((Guid)id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-           // var ce = new CalendarEvent(@event, User.FindFirstValue(ClaimTypes.NameIdentifier));
-           // return View(ce);
-        }
+                using (var db = contextFactory.CreateDbContext())
+                {
+                    //Get the assignee user (this)
+                    var user = await db.Users.FirstOrDefaultAsync(u => u.Id == identUser.Id);
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        
-        public async Task<IActionResult> Edit(Guid id, IFormCollection form)
-        {
-            try
-            {
-                //update the form dal.UpdateEvent(form);
-               // TempData["Alert"] = "Success! The event has been updated!";
-                return RedirectToAction(nameof(Index));
+                    if (user == null)
+                    {
+                        return BadRequest("User not found");
+                    }
+
+                    //Create the calendar event
+                    var calendarEvent = new CalendarEvent
+                    {
+                        Title = model.Title,
+                        Description = model.Description,
+                        StartDate = model.StartDate,
+                        EndDate = model.EndDate,
+                        Color = model.Color,
+                        CreatorId = user.Id
+                    };
+
+                    //create new GUID
+                    calendarEvent.EventId = Guid.NewGuid();
+
+                    List<EventsAssigned> assignees = new List<EventsAssigned>();
+
+                    //Assign users
+                    foreach (var userId in model.UserIds)
+                    {
+                        var assignedUser = await db.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+                        if (assignedUser == null)
+                        {
+                            Debug.WriteLine("User not found when adding calendar event");
+                            continue;
+                        }
+
+                        EventsAssigned newAssign = new EventsAssigned
+                        {
+                            EventId = calendarEvent.EventId,
+                            AssigneeId = assignedUser.Id,
+                            DateAssigned = DateTime.Now
+                        };
+
+                        assignees.Add(newAssign);
+                    }
+
+                    //Add the event to the database
+                    db.CalendarEvents.Add(calendarEvent);
+                    db.EventsAssigneds.AddRange(assignees);
+
+                    //Save the changes
+                    await db.SaveChangesAsync();
+
+                    //create new DTO
+                    var calendarEventDTO = new ReturnCalendarEventDTO
+                    {
+                        EventId = calendarEvent.EventId,
+                        Title = calendarEvent.Title,
+                        Description = calendarEvent.Description,
+                        StartDate = calendarEvent.StartDate,
+                        EndDate = calendarEvent.EndDate,
+                        Color = calendarEvent.Color,
+                        AssigneeName = user.FirstName + " " + user.LastName,
+                        AssignedUsers = new List<string>()
+                    };
+
+                    //Add the assigned users to the DTO
+                    foreach (var assignee in assignees)
+                    {
+                        var assignedUser = await db.Users.FirstOrDefaultAsync(u => u.Id == assignee.AssigneeId);
+
+                        if (assignedUser == null)
+                        {
+                            Debug.WriteLine("User not found when adding calendar event");
+                            continue;
+                        }
+
+                        calendarEventDTO.AssignedUsers.Add(assignedUser.FirstName + " " + assignedUser.LastName);
+
+                    }
+
+                    //Return the event
+                    return Ok(calendarEventDTO);
+                }
             }
             catch (Exception ex)
             {
-                //ViewData["Alert"] = "An error occurred: " + ex.Message;
-                //var ce = new CalendarEvent(dal.GetEvent(id), User.FindFirstValue(ClaimTypes.NameIdentifier));
-                //return the original event in calendar;
+                Debug.WriteLine(ex.Message);
+                return BadRequest(ex.Message);
             }
         }
 
-        // GET: Event/Delete/5
-        public IActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var @event = dal.GetEvent((Guid)id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
+        //Update calendar event
 
-           // return even in calendar;
-        }
-
-        // POST: Event/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(Guid id)
-        {
-            //to delete an event: dal.DeleteEvent(id);
-           // TempData["Alert"] = "You deleted an event.";
-            return RedirectToAction(nameof(Index));
-        }
     }
 
-    }
+}
