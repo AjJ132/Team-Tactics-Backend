f24a1b849d67e18b75969ca7c4f369956ba37ef1
Zion Magee <125425408+VaderZee@users.noreply.github.com>
2024-02-03 08:44:21 -0500

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using TeamTacticsBackend.Models.CalendarEvents;
+
+namespace Team_Tactics_Backend.Models.Users
+{
+
+public class User
+{
+    [Key]
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }    
+    public int UserType { get; set; }
+    public Guid TeamId { get; set; }
+    public DateTime DateJoined { get; set; }
+
+    public virtual ICollection <CalendarEvent> Events { get; set; }
+}
+
+ 
+}
