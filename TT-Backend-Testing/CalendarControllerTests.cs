138d0b0cc7e3d98cd73a0c876db0a6c7ff21f934
ajjohnson <aj132@icloud.com>
2024-02-06 08:41:46 -0500

    Refactor CalendarControllerTests class and add a test for creating a new calendar event

--- TT-Backend-Testing/CalendarControllerTests.cs
+++ TT-Backend-Testing/CalendarControllerTests.cs
@@ -19,94 +19,14 @@ namespace TT_Backend_Testing
 {
     public class CalendarControllerTests
     {
-        private Mock<IDbContextFactory<TeamTacticsDBContext>> _contextFactoryMock;
-        private UserManager<IdentityUser> _userManagerMock;
-        private Mock<SignInManager<IdentityUser>> _signInManagerMock;
+        private Mock<UserManager<IdentityUser>> _userManagerMock;
 
-        private CalendarController _calendarController;
-        private AuthController _authController;
+        private Mock<IDbContextFactory<TeamTacticsDBContext>> _contextFactoryMock;
 
         public CalendarControllerTests()
         {
-            _userManagerMock = new UserManager<IdentityUser>(new UserStore<IdentityUser>(new TeamTacticsDBContext(new DbContextOptions<TeamTacticsDBContext>())), null, null, null, null, null, null, null, null);
-            _signInManagerMock = new Mock<SignInManager<IdentityUser>>(_userManagerMock, null, null, null, null, null, null);
+            _userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
             _contextFactoryMock = new Mock<IDbContextFactory<TeamTacticsDBContext>>();
-
-            _authController = new AuthController(_userManagerMock, _signInManagerMock.Object, _contextFactoryMock.Object);
-            
-            GenerateUsers();
-            VerifyData();
-
-            _calendarController = new CalendarController(_userManagerMock, _contextFactoryMock.Object);
-
-            Debug.WriteLine("Setup Complete");
-        }
-
-
-        private async Task GenerateUsers()
-        {
-            var options = new DbContextOptionsBuilder<TeamTacticsDBContext>()
-                .UseInMemoryDatabase(databaseName: "TestDB")
-                .Options;
-
-            // Create a new DbContext with the in-memory database
-            var context = new TeamTacticsDBContext(options);
-
-            var teams = await GenerateTestUsers.GenerateTeams();
-            var users = await GenerateTestUsers.GenerateUsers();
-
-            foreach (var user in users)
-            {
-                var identUser = new IdentityUser
-                {
-                    Id = user.Id,
-                    UserName = user.FirstName + user.LastName,
-                    Email = user.FirstName + "@gmail.com",
-                };
-
-                var newUserID = await _authController.Register(new RegisterModel
-                {
-                    Email = user.FirstName + "@gmail.com",
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Password = "Password123!"
-                });
-
-                user.Id = newUserID.ToString();
-
-                //update team id in db
-                var dbUser = await context.Users.FirstOrDefaultAsync(n => n.Id == user.Id);
-                dbUser.TeamId = user.TeamId;
-
-                await context.SaveChangesAsync();
-            }
-
-            foreach (var team in teams)
-            {
-                await context.Teams.AddAsync(team);
-            }
-
-            await context.SaveChangesAsync();
-
-            // Mock the DbContextFactory to return the new DbContext
-            _contextFactoryMock.Setup(m => m.CreateDbContext()).Returns(new TeamTacticsDBContext(options));
-        }
-
-        private async Task VerifyData()
-        {
-            // Access the custom Users table
-            var customUsers = await _contextFactoryMock.Object.CreateDbContext().Users.ToListAsync();
-            if(customUsers.Count != 12)
-            {
-                Assert.Fail("Users not created");
-            }
-
-            var identUsers = await _userManagerMock.Users.ToListAsync();
-
-            if(identUsers.Count != 12)
-            {
-                Assert.Fail("Users not created");
-            }
         }
 
 
@@ -114,44 +34,82 @@ namespace TT_Backend_Testing
         public async Task TestCreation_General()
         {
             //SCENARIO: A team owner(coach) creates a new event for the people on their team
+            // Arrange
+            var options = new DbContextOptionsBuilder<TeamTacticsDBContext>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(databaseName: "CreateCalendarEventDB").Options;
+            var context = new TeamTacticsDBContext(options);
 
-            //Grab team1 id 
-            var team1ID = GenerateTestUsers.TeamID1;
-            var team1OwnerID = await _contextFactoryMock.Object.CreateDbContext().Teams.Where(n => n.TeamId == team1ID).Select(n => n.OwnerId).FirstOrDefaultAsync();
+            _contextFactoryMock.Setup(f => f.CreateDbContext())
+                    .Returns(new TeamTacticsDBContext(options));
 
-            //grab all users on team1
-            var userIds = await _contextFactoryMock.Object.CreateDbContext().Users.Where(n => n.TeamId == team1ID).Select(n => n.Id).ToListAsync();
+            var controller = new CalendarController(_userManagerMock.Object, _contextFactoryMock.Object);
 
-            //Setup
-            var mockUserManager = new Mock<UserManager<IdentityUser>>();
-            var mockUser = await _contextFactoryMock.Object.CreateDbContext().Users.FirstOrDefaultAsync(n => n.Id == team1OwnerID);
-
-            //Create a new event
-            var newEvent = new NewCalendarEventDTO
+            //Create three users (1 coach, 1 person on same teamm, 1 person on different team)
+            var coachIU = new IdentityUser
             {
-                Title = "Test Title",
-                Description = "Test Description",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(1),
-                Color = "#FFFFFF",
-                UserIds = userIds
+                Id = "1",
+                UserName = "Coach",
+            };
+            var coachUser = new User
+            {
+                Id = "1",
+                FirstName = "Coach",
+                LastName = "User",
+                TeamId = new Guid("T1")
             };
 
-            //Act
-            var result = await _calendarController.CreateCalendarEvent(newEvent);
+            var teamUserIU = new IdentityUser
+            {
+                Id = "2",
+                UserName = "TeamUser",
+            };
+            var teamUser = new User
+            {
+                Id = "2",
+                FirstName = "Team",
+                LastName = "User",
+                TeamId = new Guid("T1")
+            };
 
-            //Assert
-            if (result is OkObjectResult)
+            var otherTeamUserIU = new IdentityUser
             {
-                
-            }
-            else
+                Id = "3",
+                UserName = "OtherTeamUser",
+            };
+            var otherTeamUser = new User
             {
-                Assert.Fail("Not OK Result");
-            }
-            
+                Id = "3",
+                FirstName = "OtherTeam",
+                LastName = "User",
+                TeamId = new Guid("T2")
+            };
+
+            await _contextFactoryMock.Object.CreateDbContext().Users.AddAsync(coachUser);
+            await _contextFactoryMock.Object.CreateDbContext().Users.AddAsync(teamUser);
+            await _contextFactoryMock.Object.CreateDbContext().Users.AddAsync(otherTeamUser);
+
+            _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            // Act
+            var newCalendarEventDTO = new NewCalendarEventDTO
+            {
+                Title = "Test Event",
+                Description = "Test Description",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddHours(1),
+                Color = "#FF0000",
+                UserIds = new List<string> { "2" }
+            };
+
+            var result = await controller.CreateCalendarEvent(newCalendarEventDTO);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+
         }
 
-       
+
     }
 }
